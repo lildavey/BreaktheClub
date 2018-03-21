d1285238efd1cc49621840c868871150e486dde6
lildavey <david@perrens.com>
2018-03-21 14:26:23 -0500

    first

---
+++ src/PowerBar.java
@@ -0,0 +1,14 @@
+import mayflower.Actor;
+
+public class PowerBar extends Actor {
+    private int percent, speed;
+    public PowerBar(int xCoord, int yCoord)
+    {
+        setLocation(xCoord,yCoord);
+    }
+
+    @Override
+    public void act() {
+
+    }
+}
