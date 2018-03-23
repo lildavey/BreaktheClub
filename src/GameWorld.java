de06e46c39e6ba7c98bc10d7d9ff5eac5c6b43dd
lildavey <david@perrens.com>
2018-03-23 14:34:27 -0500

    etsdf

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -8,17 +8,13 @@ public class GameWorld extends World {
 
      int x;
      int y;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) 
             for(int j =0;j<10;j++) {
                 y=i*33+33*j;
                 x=j*55-55*i;
-
                 addObject(new IceBlock(50, 50), 700+x, y);
-
-
             }
 
-        }
 
 
     }
