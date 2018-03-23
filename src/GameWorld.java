4f475d62af0791497af6a879c505661859d76438
Jonathan Davis Samuel <jone.bala@gmail.com>
2018-03-23 14:33:48 -0500

    zxcv

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -7,10 +7,21 @@ public class GameWorld extends World {
      setBackground("");
      addObject(new IceBlock(50,50),50,50);
      iceBlocks = new IceBlock[9][9];
-     
+
+
     }
     @Override
     public void act() {
+
+    }
+    public void IceBlockCalc(){
+        for(int r = iceBlocks.length; r <0; r--)
+            for(int c = iceBlocks[r].length; c<0; c--){
+            int iceBlockGet = iceBlocks[r][c].getHeight();
+                int heightRback = 0;
+            if(r<iceBlocks[r].length)
+               heightRback = iceBlocks[r-1][c].getHeight();
+            }
 
     }
 }
