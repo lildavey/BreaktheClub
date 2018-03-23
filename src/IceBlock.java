84fae9525356f0c2d06de2769330442cddbab702
Jonathan Davis Samuel <jone.bala@gmail.com>
2018-03-23 13:34:08 -0500

    fghj

--- src/IceBlock.java
+++ src/IceBlock.java
@@ -9,10 +9,11 @@ import java.util.List;
 
 public class IceBlock extends Actor {
     private Mayflower mayflower;
-    public int height, health;
+    public int height, health, row, col;
+    public int[][] iceblocks;
 
-    public IceBlock(int height, int health ){
-        this.height = height; this.health = health;
+    public IceBlock(int height, int row, int col, int health , int[][] iceblocks ){
+        this.height = height; this.health = health; this.row = row; this.col = col; this.iceblocks = iceblocks;
 
         this.setImage("img/Ice_Block.png");
 
@@ -23,12 +24,17 @@ public class IceBlock extends Actor {
     public void act() {
 
     }
-/*    public updateHealth(){
+  public updateHealth() {
+        if(row)
+
+    }
+
         
-    }*/
+
 
     public int getHealth() {
-        return health;
+        updateHealth();
+         return health;
     }
 
     public int getHeight() {
