aa2fda36d76ff5ecd22aea1a115382539dfdc258
trevm1226 <hipie@1226>
2018-03-23 13:31:13 -0500

    bar

--- src/PowerBar.java
+++ src/PowerBar.java
@@ -1,8 +1,6 @@
 import mayflower.Actor;
 
 public class PowerBar extends Actor {
-    private int speed,percent;
-
     public PowerBar(int newx,int newy)
     {
         setLocation(newx,newy);
@@ -16,9 +14,29 @@ public class PowerBar extends Actor {
 }
 class Bar extends Actor
 {
+    private int speedVar, percent;
+    private PowerBar thisBar;
+    public Bar(PowerBar tempBar)
+    {
+        speedVar = 1;
+        percent  = 0;
+        thisBar = tempBar;
+        setLocation(thisBar.getX(), thisBar.getY()-8);
+        setImage("bar.jpg");
+        setRotation(90);
+    }
+
+    public int getPercent()
+    {
+        int temp = getCenterX() - thisBar.getX(); percent = temp/4;
+        return percent;
+    }
 
     @Override
     public void act() {
-
+        this.getPercent();
+        if(getX() == thisBar.getX()) setRotation(90);
+        else if(getX()+15 == thisBar.getX()+400) setRotation(270);
+        move(speedVar);
     }
 }
