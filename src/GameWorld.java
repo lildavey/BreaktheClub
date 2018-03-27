a69f1fce6ea3cb39406fba93eb1c2af0ae814703
trevm1226 <hipie@1226>
2018-03-27 12:58:27 -0500

    hot

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -1,32 +1,28 @@
 import mayflower.World;
 
 public class GameWorld extends World {
-    public GameWorld()
-    {
-<<<<<<< HEAD
+    public GameWorld() {
         PowerBar narnar = new PowerBar();
-     //setBackground("");
-     addObject(new IceBlock(50,50),50,50);
-     addObject(narnar, 400, 300);
-     addObject(new Bar(narnar), 400,292);
-     iceBlocks = new IceBlock[9][9];
-=======
-     setBackground("img/tileMap.png");
+        //setBackground("");
+        addObject(new IceBlock(50, 50), 50, 50);
+        addObject(narnar, 400, 300);
+        addObject(new Bar(narnar), 400, 292);
+        iceBlocks = new IceBlock[9][9];
+        setBackground("img/tileMap.png");
 
 
-     int x;
-     int y;
+        int x;
+        int y;
         for (int i = 0; i < 10; i++)
-            for(int j =0;j<10;j++) {
-                y=i*33+33*j;
-                x=j*55-55*i;
-                addObject(new IceBlock(50, 50), 700+x, y);
+            for (int j = 0; j < 10; j++) {
+                y = i * 33 + 33 * j;
+                x = j * 55 - 55 * i;
+                addObject(new IceBlock(50, 50), 700 + x, y);
             }
 
 
-
->>>>>>> 963675840e8c15197335d82547ed0025a60da4a1
     }
+
     @Override
     public void act() {
 
