8aa6720a76bdb09fa35a6a43d6234668e6e5d0e8
Jonathan Davis Samuel <jone.bala@gmail.com>
2018-03-27 12:49:45 -0500

    fjhfgjjh

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -1,18 +1,21 @@
 import mayflower.World;
 
 public class GameWorld extends World {
+    public IceBlock[][] iceBlocks;
     public GameWorld()
     {
      setBackground("img/tileMap.png");
-
+        iceBlocks = new IceBlock[9][9];
 
      int x;
      int y;
-        for (int i = 0; i < 10; i++)
-            for(int j =0;j<10;j++) {
+        for (int i = 0; i < 9; i++)
+            for(int j =0;j<9;j++) {
                 y=i*33+33*j;
                 x=j*55-55*i;
-                addObject(new IceBlock(50, 50), 700+x, y);
+                IceBlock newOne = new IceBlock(50, 50);
+                iceBlocks[i][j] = newOne;
+                addObject(newOne, 700+x, 50+y);
             }
 
 
@@ -22,4 +25,15 @@ public class GameWorld extends World {
     public void act() {
 
     }
+    public void IceBlockCalc(){
+        for(int r = iceBlocks.length; r >0; r--)
+            for(int c = iceBlocks[r].length; c>0; c--){
+                int iceBlockGet = iceBlocks[r][c].getHeight();
+                int heightRback = 0;
+                if(r<iceBlocks[r].length)
+                    heightRback = iceBlocks[r-1][c].getHeight();
+            }
+
+    }
+
 }
